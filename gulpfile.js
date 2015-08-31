b69ba1d3e1d6aa8df3339ea960b2bc7d77f120d1
atask <atask@users.noreply.github.com>
2015-09-01 01:14:11 +0200

    add source maps generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp'),
     merge = require('merge-stream'),
+    sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel');
 
 gulp.task('default', function() {
   var index = gulp.src('index.es6')
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('.'));
 
   var methane = gulp.src('lib/methane.es6')
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./lib'));
 
   var readers = gulp.src('lib/readers/*.es6')
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./lib/readers'));
 
   return merge(index, methane, readers);
